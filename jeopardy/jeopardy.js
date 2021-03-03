38a2835378f1e1ae552423a48e8047df3027a5d1
mits <dmitrybaycharov@gmail.com>
2021-03-03 17:20:32 -0500

    mostly finished

--- jeopardy/jeopardy.js
+++ jeopardy/jeopardy.js
@@ -20,6 +20,7 @@
 
 let categories = [];
 const NUM_CATEGORIES = 6;
+const NUM_QUESTIONS_PER_CAT = 5;
 
 /** Get NUM_CATEGORIES random category from API.
  *
@@ -48,6 +49,7 @@ async function getCategoryIds() {
 async function getCategory(catId) {
     let res = await axios.get(`https://jservice.io/api/category?id=${catId}`);
     let categoryData = res.data;
+
     return {
         title: categoryData.title,
         clues: categoryData.clues
@@ -63,7 +65,20 @@ async function getCategory(catId) {
  */
 
 async function fillTable() {
-    
+    for(category of categories){
+        $('#headerRow').append(`<td class="cell header-cell">${category.title}</td>`);
+    }
+ 
+    //append tr from top to bottom
+        //append td from left to right 
+    for(let rowIdx=0; rowIdx<NUM_QUESTIONS_PER_CAT; rowIdx++){
+        $('#gameBody').append(`<tr data-row="${rowIdx}"></tr>`);
+
+        for(let colIdx=0; colIdx<categories.length; colIdx++){            
+            $(`#gameBody tr[data-row='${rowIdx}']`).append(`<td class="cell" data-col="${colIdx}" data-showing="hidden">?</td>`);
+        }
+    }
+ 
 }
 
 /** Handle clicking on a clue: show the question or answer.
@@ -73,8 +88,23 @@ async function fillTable() {
  * - if currently "question", show answer & set .showing to "answer"
  * - if currently "answer", ignore click
  * */
-
+ 
 function handleClick(evt) {
+    let clickedCell = evt.target;
+    let questionDifficulty = clickedCell.parentNode.dataset.row;
+    // if currently null, show question & set .showing to "question" data attr? 
+    if(clickedCell.dataset.showing==="hidden") {
+        clickedCell.dataset.showing = "question";
+        let catCol = clickedCell.dataset.col;
+        clickedCell.innerHTML = categories[catCol].clues[questionDifficulty].question;
+    }
+    // if currently "question", show answer & set .showing to "answer"
+    else if(clickedCell.dataset.showing==="question") {
+        clickedCell.dataset.showing = "answer";  
+        let catCol = clickedCell.dataset.col;
+        clickedCell.innerHTML = categories[catCol].clues[questionDifficulty].answer;
+    }
+    // if currently "answer", ignore click
 }
 
 /** Wipe the current Jeopardy board, show the loading spinner,
@@ -82,12 +112,18 @@ function handleClick(evt) {
  */
 
 function showLoadingView() {
-
+    let bootstrapSpinner = `<div id="spinner" class="spinner-border text-info" role="status">
+    <span class="visually-hidden">Loading...</span></div>`;
+    let spinnerEl = document.createElement("div");
+    spinnerEl.innerHTML = bootstrapSpinner;
+    document.querySelector('body').append(spinnerEl);
 }
 
 /** Remove the loading spinner and update the button used to fetch data. */
 
 function hideLoadingView() {
+    let spinnerEl = document.getElementById('spinner')
+    if(spinnerEl) spinnerEl.remove();
 }
 
 /** Start game:
@@ -98,12 +134,31 @@ function hideLoadingView() {
  * */
 
 async function setupAndStart() {
+    let categoryIds = await getCategoryIds();
+
+    for(catIdx=0; catIdx<categoryIds.length; catIdx++){
+        let curCategoryInfo = await getCategory(categoryIds[catIdx])
+        categories.push(curCategoryInfo);
+    }
+
+    fillTable();
 }
 
 /** On click of start / restart button, set up game. */
 
 // TODO
+$('#gameBtn').on('click', async function(){
+    $('#gameBtn').prop("disabled", true); //prevents from spamming the categories array
+    $('#headerRow').empty();
+    $('#gameBody').empty();
+    categories = [];
+    showLoadingView();
+    await setupAndStart();
+    hideLoadingView();
+    $('#gameBtn').prop("disabled", false);
+})
 
 /** On page load, add event handler for clicking clues */
 
 // TODO
+$('#gameBody').on('click', 'td', handleClick);
